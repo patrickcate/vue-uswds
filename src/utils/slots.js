7a39e09a3b9cc319ab1b8b71f42fa3254497c6ec
Patrick Cate <patrickcate@users.noreply.github.com>
2021-11-07 09:31:10 -0500

    refactor: return early if slot is not defined

--- src/utils/slots.js
+++ src/utils/slots.js
@@ -1,16 +1,16 @@
 export function slotHasContent(slot, data) {
-  return (
-    slot &&
-    slot(data) &&
-    slot(data).filter(vnode => {
-      return (
-        vnode.type.toString() !== 'Symbol(Comment)' &&
-        vnode.type.toString() !== 'Comment' &&
-        vnode.children !== null &&
-        (Array.isArray(vnode.children) ||
-          typeof vnode.children === 'object' ||
-          (typeof vnode.children === 'string' && vnode.children.trim() !== ''))
-      )
-    }).length
-  )
+  if (!slot || !slot(data)) {
+    return false
+  }
+
+  return !!slot(data).filter(vnode => {
+    return (
+      vnode.type.toString() !== 'Symbol(Comment)' &&
+      vnode.type.toString() !== 'Comment' &&
+      vnode.children !== null &&
+      (Array.isArray(vnode.children) ||
+        typeof vnode.children === 'object' ||
+        (typeof vnode.children === 'string' && vnode.children.trim() !== ''))
+    )
+  }).length
 }
