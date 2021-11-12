97ed78ec8cc3ef94b3fd1155f1de2d71cbbacebf
Patrick Cate <patrickcate@users.noreply.github.com>
2021-11-11 23:46:32 -0500

    ci: change ie browserlist entry causing Cypress e2e tests to fail

--- cypress/integration/init.test.js
+++ cypress/integration/init.test.js
@@ -1,3 +1,5 @@
 describe('cypress runs', () => {
-  it('cypress does not fail because of no test files in integration folder', () => {})
+  it('cypress does not fail because of no test files in integration folder', () => {
+    expect(true).to.equal(true)
+  })
 })
