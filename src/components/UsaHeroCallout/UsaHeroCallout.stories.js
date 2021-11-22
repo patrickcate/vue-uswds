f9ee2085cbc17bcb6bdeedd57483d4fa497abd47
Patrick Cate <patrickcate@users.noreply.github.com>
2021-11-22 14:59:09 -0500

    feat: implement `UsaHeroCallout` component
    ISSUES CLOSED: #41

---
+++ src/components/UsaHeroCallout/UsaHeroCallout.stories.js
@@ -0,0 +1,110 @@
+import UsaHeroCallout from './UsaHeroCallout.vue'
+import UsaHero from '@/components/UsaHero'
+
+const defaultProps = {
+  headingAlt: 'Hero callout:',
+  heading: 'Bring attention to a project priority',
+  headingTag: 'h1',
+  customClasses: {
+    heading: [],
+    headingAlt: [],
+  },
+}
+
+export default {
+  component: UsaHeroCallout,
+  title: 'Components/UsaHeroCallout',
+  argTypes: {
+    headingTag: {
+      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+      control: {
+        type: 'select',
+      },
+    },
+    headingAlt: {
+      control: { type: 'text' },
+    },
+    heading: {
+      control: { type: 'text' },
+    },
+    customClasses: {
+      control: { type: 'object' },
+    },
+    headingAltSlot: {
+      control: { type: 'text' },
+    },
+    headingSlot: {
+      control: { type: 'text' },
+    },
+    defaultSlot: {
+      control: { type: 'text' },
+    },
+  },
+  args: {
+    headingTag: defaultProps.headingTag,
+    headingAlt: defaultProps.headingAlt,
+    heading: defaultProps.heading,
+    customClasses: defaultProps.customClasses,
+    headingAltSlot: '',
+    headingSlot: '',
+    defaultSlot:
+      "<p>Support the callout with some short explanatory text. You don't need more than a couple of sentences.</p>",
+  },
+}
+
+const DefaultTemplate = (args, { argTypes }) => ({
+  components: { UsaHeroCallout, UsaHero },
+  props: Object.keys(argTypes),
+  setup() {
+    return { ...args }
+  },
+  template: `<UsaHero background-image="'https://federalist-3b6ba08e-0df4-44c9-ac73-6fc193b0e19c.app.cloud.gov/preview/uswds/uswds/develop/dist/img/hero.png'" aria-label="Introduction">
+    <UsaHeroCallout
+      :heading="heading"
+      :heading-alt="headingAlt"
+      :heading-tag="headingTag"
+      :custom-classes="customClasses"
+    >
+      <template #headingAlt>${args.headingAltSlot}</template>
+      <template #heading>${args.headingSlot}</template>
+      <template #default>${args.defaultSlot}</template>
+    </UsaHeroCallout>
+  </UsaHero>`,
+})
+
+export const DefaultHeroCallout = DefaultTemplate.bind({})
+DefaultHeroCallout.args = {
+  ...defaultProps,
+}
+DefaultHeroCallout.storyName = 'Default'
+
+export const HeadingSlotHeroCallout = DefaultTemplate.bind({})
+HeadingSlotHeroCallout.args = {
+  ...defaultProps,
+  headingSlot: 'Custom heading slot',
+}
+HeadingSlotHeroCallout.storyName = 'Heading Slot'
+
+export const HeadingAltSlotHeroCallout = DefaultTemplate.bind({})
+HeadingAltSlotHeroCallout.args = {
+  ...defaultProps,
+  headingAltSlot: 'Custom heading alt slot',
+}
+HeadingAltSlotHeroCallout.storyName = 'Heading Alt Slot'
+
+export const CustomHeadingHeroCallout = DefaultTemplate.bind({})
+CustomHeadingHeroCallout.args = {
+  ...defaultProps,
+  headingTag: 'h4',
+}
+CustomHeadingHeroCallout.storyName = 'Custom Heading Tag'
+
+export const CustomClassesHeroCallout = DefaultTemplate.bind({})
+CustomClassesHeroCallout.args = {
+  ...defaultProps,
+  customClasses: {
+    heading: ['test-heading-class'],
+    headingAlt: ['test-heading-alt-class'],
+  },
+}
+CustomClassesHeroCallout.storyName = 'Custom Classes'
---
+++ src/components/UsaHeroCallout/UsaHeroCallout.test.js
@@ -0,0 +1,86 @@
+import '@module/uswds/dist/css/uswds.min.css'
+import { h } from 'vue'
+import { mount } from '@cypress/vue'
+import UsaHeroCallout from './UsaHeroCallout.vue'
+
+describe('UsaHeroCallout', () => {
+  const testHeadingAlt = 'Hero callout:'
+  const testHeading = 'Bring attention to a project priority'
+  const textContent =
+    "Support the callout with some short explanatory text. You don't need more than a couple of sentences."
+
+  it('renders the component', () => {
+    mount(UsaHeroCallout, {
+      props: {
+        headingAlt: testHeadingAlt,
+        heading: testHeading,
+      },
+      slots: {
+        default: () => h('p', null, textContent),
+      },
+    })
+
+    cy.get('.usa-hero__callout').should('exist')
+    cy.get('h1.usa-hero__heading').should('contain', testHeading)
+    cy.get('.usa-hero__heading--alt').should('contain', testHeadingAlt)
+    cy.get('.usa-hero__callout p').should('contain', textContent)
+  })
+
+  it('renders `heading` and `headingAlt` slot content', () => {
+    mount(UsaHeroCallout, {
+      props: {
+        headingAlt: testHeadingAlt,
+        heading: testHeading,
+      },
+      slots: {
+        headingAlt: () => 'Custom heading alt slot content',
+        heading: () => 'Custom heading slot content',
+        default: () => h('p', null, textContent),
+      },
+    })
+
+    cy.get('.usa-hero__heading').should(
+      'contain',
+      'Custom heading slot content'
+    )
+
+    cy.get('.usa-hero__heading--alt').should(
+      'contain',
+      'Custom heading alt slot content'
+    )
+  })
+
+  it('renders custom heading tag', () => {
+    mount(UsaHeroCallout, {
+      props: {
+        headingAlt: testHeadingAlt,
+        heading: testHeading,
+        headingTag: 'h4',
+      },
+      slots: {
+        default: () => h('p', null, textContent),
+      },
+    })
+
+    cy.get('h4.usa-hero__heading')
+  })
+
+  it('adds custom CSS classes', () => {
+    mount(UsaHeroCallout, {
+      props: {
+        headingAlt: testHeadingAlt,
+        heading: testHeading,
+        customClasses: {
+          headingAlt: ['test-heading-alt-class'],
+          heading: ['test-heading-class'],
+        },
+      },
+      slots: {
+        default: () => h('p', null, textContent),
+      },
+    })
+
+    cy.get('.test-heading-alt-class').should('exist')
+    cy.get('.test-heading-class').should('exist')
+  })
+})
