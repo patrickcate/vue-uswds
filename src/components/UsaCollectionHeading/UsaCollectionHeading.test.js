3c9df87426cb9487c16d4f17c42ee5789dfbb854
Patrick Cate <patrickcate@users.noreply.github.com>
2021-11-23 13:32:28 -0500

    feat: implement `UsaCollectionHeading` component
    ISSUES CLOSED: #20

---
+++ src/components/UsaCollectionHeading/UsaCollectionHeading.test.js
@@ -0,0 +1,71 @@
+import '@module/uswds/dist/css/uswds.min.css'
+import { mount } from '@cypress/vue'
+import UsaCollectionHeading from './UsaCollectionHeading.vue'
+
+describe('UsaCollectionHeading', () => {
+  it('renders the component', () => {
+    mount(UsaCollectionHeading, {
+      props: {
+        header: 'Test header text',
+        href: '/test-page',
+      },
+    })
+
+    cy.get('h2.usa-collection__heading a')
+      .should('have.class', 'usa-link')
+      .and('have.attr', 'href')
+      .and('contain', '/test-page')
+  })
+
+  it('uses the `heading` slot content', () => {
+    mount(UsaCollectionHeading, {
+      props: {
+        header: 'Test header text',
+      },
+      slots: {
+        default: () => 'Custom slot heading text',
+      },
+    })
+
+    cy.get('.usa-collection__heading').should(
+      'contain',
+      'Custom slot heading text'
+    )
+  })
+
+  it('renders custom heading tag', () => {
+    mount(UsaCollectionHeading, {
+      props: {
+        header: 'Test header text',
+        headingTag: 'h4',
+      },
+    })
+
+    cy.get('h4.usa-collection__heading').should('exist')
+  })
+
+  it('renders the custom router link tag and props', () => {
+    mount(UsaCollectionHeading, {
+      props: {
+        heading: 'Test header text',
+        to: '/test-page',
+        routerComponentName: 'router-link',
+      },
+    })
+
+    cy.get('router-link').should('have.attr', 'to').and('contain', '/test-page')
+  })
+
+  it('adds custom CSS classes', () => {
+    mount(UsaCollectionHeading, {
+      props: {
+        header: 'Test header text',
+        customClasses: {
+          link: ['test-link-class'],
+        },
+      },
+    })
+
+    cy.get('a.test-link-class').should('exist')
+  })
+})
