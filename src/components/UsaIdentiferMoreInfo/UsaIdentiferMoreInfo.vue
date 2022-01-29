baae042f73083a0d4739afd2f89a1d0aa31b01e9
Patrick Cate <patrickcate@users.noreply.github.com>
2022-01-29 00:06:59 -0500

    feat(UsaIdentiferMoreInfo): implement `UsaIdentifierMoreInfo` component
    ISSUES CLOSED: #45

---
+++ src/components/UsaIdentiferMoreInfo/UsaIdentiferMoreInfo.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  ariaLabel: {
+    type: String,
+    default: 'U.S. government information and services',
+  },
+  description: {
+    type: String,
+    default: 'Looking for U.S. government information and services?',
+  },
+  linkText: {
+    type: String,
+    default: 'Visit USA.gov',
+  },
+  linkUrl: {
+    type: String,
+    default: 'https://www.usa.gov',
+  },
+})
+</script>
+
+<template>
+  <section
+    class="usa-identifier__section usa-identifier__section--usagov"
+    :aria-label="ariaLabel"
+  >
+    <div class="usa-identifier__container">
+      <div class="usa-identifier__usagov-description">{{ description }}</div
+      >&nbsp;
+      <a :href="linkUrl" class="usa-link">{{ linkText }}</a>
+    </div>
+  </section>
+</template>
---
+++ src/components/UsaIdentiferMoreInfo/index.js
@@ -0,0 +1,4 @@
+import UsaIdentiferMoreInfo from './UsaIdentiferMoreInfo.vue'
+
+export { UsaIdentiferMoreInfo }
+export default UsaIdentiferMoreInfo
