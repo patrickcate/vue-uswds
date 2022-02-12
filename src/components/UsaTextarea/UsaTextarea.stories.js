d279ef9efbbda61a04cf4ba641ed28e4ae50af2b
Patrick Cate <patrickcate@users.noreply.github.com>
2022-02-12 17:26:55 -0500

    feat(UsaTextarea): implement `UsaTextarea` component
    ISSUES CLOSED: #50

---
+++ src/components/UsaTextarea/UsaTextarea.stories.js
@@ -0,0 +1,186 @@
+import UsaTextarea from './UsaTextarea.vue'
+
+const defaultProps = {
+  width: UsaTextarea.props.width.default,
+  modelValue: UsaTextarea.props.modelValue.default,
+  label: UsaTextarea.props.label.default,
+  required: UsaTextarea.props.required.default,
+  error: UsaTextarea.props.error.default,
+  id: UsaTextarea.props.id.default,
+  customClasses: UsaTextarea.props.customClasses.default(),
+}
+
+export default {
+  component: UsaTextarea,
+  title: 'Components/UsaTextarea',
+  argTypes: {
+    width: {
+      options: [
+        '',
+        '2xs',
+        'xs',
+        'sm',
+        'small',
+        'md',
+        'medium',
+        'lg',
+        'xl',
+        '2xl',
+      ],
+      control: {
+        type: 'select',
+        labels: {
+          '': '- none -',
+        },
+      },
+    },
+    modelValue: {
+      control: { type: 'text' },
+    },
+    label: {
+      control: { type: 'text' },
+    },
+    required: {
+      control: { type: 'boolean' },
+    },
+    error: {
+      control: { type: 'boolean' },
+    },
+    id: {
+      control: { type: 'text' },
+    },
+    customClasses: {
+      control: { type: 'object' },
+    },
+    labelSlot: {
+      control: { type: 'text' },
+    },
+    hintSlot: {
+      control: { type: 'text' },
+    },
+    errorMessageSlot: {
+      control: { type: 'text' },
+    },
+  },
+  args: {
+    width: defaultProps.width,
+    modelValue: defaultProps.modelValue,
+    label: defaultProps.label,
+    required: defaultProps.required,
+    error: defaultProps.error,
+    id: defaultProps.id,
+    customClasses: defaultProps.customClasses,
+    labelSlot: '',
+    hintSlot: '',
+    errorMessageSlot: '',
+  },
+  decorators: [
+    () => ({
+      template: '<div class="usa-form usa-form--large"><story /></div>',
+    }),
+  ],
+}
+
+const DefaultTemplate = (args, { argTypes }) => ({
+  components: { UsaTextarea },
+  props: Object.keys(argTypes),
+  setup() {
+    return { ...args }
+  },
+  template: `<UsaTextarea
+    :width="width"
+    :label="label"
+    :required="required"
+    :error="error"
+    :id="id"
+    :custom-classes="customClasses"
+    v-model="modelValue"
+    v-bind="$attrs"
+  >
+    <template #label>${args.labelSlot}</template>
+    <template #hint>${args.hintSlot}</template>
+    <template #error-message>${args.errorMessageSlot}</template>
+  </UsaTextarea>`,
+})
+
+export const DefaultTextarea = DefaultTemplate.bind({})
+DefaultTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+}
+DefaultTextarea.storyName = 'Default'
+
+export const DefaultTextTextarea = DefaultTemplate.bind({})
+DefaultTextTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+  modelValue: `I'm some default text.`,
+}
+DefaultTextTextarea.storyName = 'Default Text'
+
+export const WidthTextarea = DefaultTemplate.bind({})
+WidthTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+  width: '2xl',
+}
+WidthTextarea.storyName = 'Width'
+
+export const HintTextarea = DefaultTemplate.bind({})
+HintTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+  hintSlot: 'Choose wisely',
+}
+HintTextarea.storyName = 'Hint'
+
+export const ErrorTextarea = DefaultTemplate.bind({})
+ErrorTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+  error: true,
+}
+ErrorTextarea.storyName = 'Error'
+
+export const ErrorMessageTextarea = DefaultTemplate.bind({})
+ErrorMessageTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+  error: true,
+  errorMessageSlot: 'Error message here',
+}
+ErrorMessageTextarea.storyName = 'Error Message'
+
+export const RequiredTextarea = DefaultTemplate.bind({})
+RequiredTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+  required: true,
+}
+RequiredTextarea.storyName = 'Required'
+
+export const LabelSlotTextarea = DefaultTemplate.bind({})
+LabelSlotTextarea.args = {
+  ...defaultProps,
+  labelSlot: `<em>Label slot content</em>`,
+}
+LabelSlotTextarea.storyName = 'Label Slot'
+
+export const CustomIdTextarea = DefaultTemplate.bind({})
+CustomIdTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+  id: 'custom-id',
+}
+CustomIdTextarea.storyName = 'Custom ID'
+
+export const CustomClassesTextarea = DefaultTemplate.bind({})
+CustomClassesTextarea.args = {
+  ...defaultProps,
+  label: 'Textarea label',
+  customClasses: {
+    component: ['test-component-class'],
+    label: ['test-label-class'],
+  },
+}
+CustomClassesTextarea.storyName = 'Custom CSS Classes'
---
+++ src/components/UsaTextarea/UsaTextarea.vue
@@ -0,0 +1,150 @@
+<script>
+export default {
+  inheritAttrs: false,
+}
+</script>
+
+<script setup>
+import { computed, useSlots } from 'vue'
+import { nextId } from '@/utils/unique-id.js'
+import UsaLabel from '@/components/UsaLabel'
+
+const slots = useSlots()
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps({
+  width: {
+    type: String,
+    default: '',
+    validator(width) {
+      const isValidWidth = [
+        '',
+        '2xs',
+        'xs',
+        'sm',
+        'small',
+        'md',
+        'medium',
+        'lg',
+        'xl',
+        '2xl',
+      ].includes(width)
+
+      if (!isValidWidth) {
+        console.warn(`'${width}' is not a valid textarea width`)
+      }
+
+      return isValidWidth
+    },
+  },
+  modelValue: {
+    type: undefined,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  error: {
+    type: Boolean,
+    default: false,
+  },
+  id: {
+    type: String,
+    default: '',
+  },
+  customClasses: {
+    type: Object,
+    default: () => {
+      return {
+        component: [],
+        label: [],
+      }
+    },
+  },
+})
+
+const computedId = computed(() => props.id || nextId('usa-textarea'))
+const computedErrorMessageId = computed(
+  () => `${computedId.value}-error-message`
+)
+const computedHintId = computed(() => `${computedId.value}-hint`)
+
+const textareaValue = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  },
+})
+
+const classes = computed(() => [
+  { 'usa-input--error': props.error },
+  { 'usa-input--2xs': props.width === '2xs' },
+  { 'usa-input--xs': props.width === 'xs' },
+  { 'usa-input--sm': props.width === 'sm' },
+  { 'usa-input--small': props.width === 'small' },
+  { 'usa-input--md': props.width === 'md' },
+  { 'usa-input--medium': props.width === 'medium' },
+  { 'usa-input--lg': props.width === 'lg' },
+  { 'usa-input--xl': props.width === 'xl' },
+  { 'usa-input--2xl': props.width === '2xl' },
+])
+
+const formGroupClasses = computed(() => [
+  { 'usa-form-group--error': props.error },
+  ...(props.customClasses?.component || []),
+])
+
+const ariaDescribedby = computed(() => {
+  const ids = []
+
+  if (slots.hint) {
+    ids.push(computedHintId.value)
+  }
+
+  if (props.error && slots['error-message']) {
+    ids.push(computedErrorMessageId.value)
+  }
+
+  return ids.length ? ids.join(' ') : null
+})
+</script>
+
+<template>
+  <div class="usa-form-group" :class="formGroupClasses">
+    <UsaLabel
+      :for="computedId"
+      :required="required"
+      :error="error"
+      :class="customClasses?.label"
+      ><slot name="label">{{ label }}</slot></UsaLabel
+    >
+
+    <span v-if="$slots.hint" :id="computedHintId" class="usa-hint"
+      ><slot name="hint"></slot
+    ></span>
+
+    <span
+      v-if="error && $slots['error-message']"
+      :id="computedErrorMessageId"
+      class="usa-error-message"
+      ><slot name="error-message"></slot
+    ></span>
+
+    <textarea
+      :id="computedId"
+      v-model="textareaValue"
+      class="usa-textarea"
+      :class="classes"
+      :required="required"
+      :aria-describedby="ariaDescribedby"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
