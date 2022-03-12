31acdb7762866cb4261ff89ce5455fec37268452
Patrick Cate <patrickcate@users.noreply.github.com>
2022-03-12 00:23:41 -0500

    feat(UsaNavDropdownButton): implement `UsaNavDropdownButton` component

---
+++ src/components/UsaNavDropdownButton/UsaNavDropdownButton.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  inheritAttrs: false,
+}
+</script>
+
+<script setup>
+import { inject, toRef } from 'vue'
+import { ROUTER_COMPONENT_NAME } from '@/utils/constants.js'
+
+const routerComponentName = inject(
+  'vueUswds.routerComponentName',
+  ROUTER_COMPONENT_NAME
+)
+
+const dropdownId = inject('dropdownId')
+const toggleDropdown = inject('toggleDropdown')
+const dropdownItems = inject('dropdownItems')
+const isOpen = toRef(dropdownItems, dropdownId.value)
+</script>
+
+<template>
+  <component
+    :is="routerComponentName"
+    v-if="routerComponentName"
+    v-slot="{ isActive, isExactActive }"
+    custom
+  >
+    <button
+      v-bind="$attrs"
+      class="usa-accordion__button usa-nav__link"
+      :class="[{ 'usa-current': isActive || isExactActive }]"
+      :aria-expanded="isOpen"
+      :aria-controls="dropdownId"
+      @click="toggleDropdown(dropdownId)"
+    >
+      <span><slot></slot></span>
+    </button>
+  </component>
+  <button
+    v-else
+    v-bind="$attrs"
+    class="usa-accordion__button usa-nav__link"
+    :aria-expanded="isOpen"
+    :aria-controls="dropdownId"
+    @click="toggleDropdown(dropdownId)"
+  >
+    <span><slot></slot></span>
+  </button>
+</template>
