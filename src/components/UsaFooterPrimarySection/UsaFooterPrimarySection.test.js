3bf9c4ba3a0d5fb8cf118ff4aa337830c6206b07
Patrick Cate <patrickcate@users.noreply.github.com>
2022-04-18 00:14:58 -0400

    feat(UsaFooterPrimarySection): implement `UsaFooterPrimarySection` component
    ISSUES CLOSED: #33

---
+++ src/components/UsaFooterPrimarySection/UsaFooterPrimarySection.test.js
@@ -0,0 +1,18 @@
+import '@module/uswds/dist/css/uswds.min.css'
+import { mount } from '@cypress/vue'
+import UsaFooterPrimarySection from './UsaFooterPrimarySection.vue'
+
+describe('UsaFooterPrimarySection', () => {
+  it('renders the component', () => {
+    mount(UsaFooterPrimarySection, {
+      slots: {
+        default: () => 'Test primary slot content',
+      },
+    })
+
+    cy.get('div.usa-footer__primary-section').should(
+      'contain',
+      'Test primary slot content'
+    )
+  })
+})
