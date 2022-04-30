e4ac72209043bbf516a499b8e54041c5eb5003f7
Patrick Cate <patrickcate@users.noreply.github.com>
2022-04-30 00:27:32 -0400

    build: add playground project to test tree shaking

---
+++ playground/tree-shaking/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import VueUswds from 'vue-uswds/core'
+import App from './App.vue'
+
+createApp(App).use(VueUswds).mount('#app')
---
+++ playground/tree-shaking/vite.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    minify: false,
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
