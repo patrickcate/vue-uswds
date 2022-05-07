7645240ee072677fe30406eaae42bebbdc3c121a
Patrick Cate <patrickcate@users.noreply.github.com>
2022-05-07 15:05:54 -0400

    test(BaseHeading): add example stories for different heading tags

--- src/components/BaseHeading/BaseHeading.stories.js
+++ src/components/BaseHeading/BaseHeading.stories.js
@@ -20,7 +20,7 @@ export default {
   },
   args: {
     tag: defaultProps.tag,
-    defaultSlot: 'Test',
+    defaultSlot: '',
   },
 }
 
@@ -36,5 +36,54 @@ const DefaultTemplate = (args, { argTypes }) => ({
 export const DefaultBaseHeading = DefaultTemplate.bind({})
 DefaultBaseHeading.args = {
   ...defaultProps,
+  defaultSlot: 'Default Heading',
 }
 DefaultBaseHeading.storyName = 'Default'
+
+export const Level1BaseHeading = DefaultTemplate.bind({})
+Level1BaseHeading.args = {
+  ...defaultProps,
+  defaultSlot: 'h1 Heading',
+  tag: 'h1',
+}
+Level1BaseHeading.storyName = 'Heading Level 1'
+
+export const Level2BaseHeading = DefaultTemplate.bind({})
+Level2BaseHeading.args = {
+  ...defaultProps,
+  defaultSlot: 'h2 Heading',
+  tag: 'h2',
+}
+Level2BaseHeading.storyName = 'Heading Level 2'
+
+export const Level3BaseHeading = DefaultTemplate.bind({})
+Level3BaseHeading.args = {
+  ...defaultProps,
+  defaultSlot: 'h3 Heading',
+  tag: 'h3',
+}
+Level3BaseHeading.storyName = 'Heading Level 3'
+
+export const Level4BaseHeading = DefaultTemplate.bind({})
+Level4BaseHeading.args = {
+  ...defaultProps,
+  defaultSlot: 'h4 Heading',
+  tag: 'h4',
+}
+Level4BaseHeading.storyName = 'Heading Level 4'
+
+export const Level5BaseHeading = DefaultTemplate.bind({})
+Level5BaseHeading.args = {
+  ...defaultProps,
+  defaultSlot: 'h5 Heading',
+  tag: 'h5',
+}
+Level5BaseHeading.storyName = 'Heading Level 5'
+
+export const Level6BaseHeading = DefaultTemplate.bind({})
+Level6BaseHeading.args = {
+  ...defaultProps,
+  defaultSlot: 'h6 Heading',
+  tag: 'h6',
+}
+Level6BaseHeading.storyName = 'Heading Level 6'
